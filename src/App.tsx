3ff991e449a9e8ea6155c183b7ff2712ecdbe0d3
Shadman Shakib <sm.shakib007@gmail.com>
2021-09-22 11:55:55 +0600

    Dayjs added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import dayjs from "dayjs";
+import { Box, Text } from "@chakra-ui/react";
+import dayoftheyear from "dayjs/plugin/dayOfYear";
+import utc from "dayjs/plugin/utc";
+import weekOfTheYear from "dayjs/plugin/weekOfYear";
 
 function App() {
+  dayjs.extend(dayoftheyear);
+  dayjs.extend(utc);
+  dayjs.extend(weekOfTheYear);
+  const localDate = dayjs().local();
+  const today = dayjs(localDate).dayOfYear();
+  const thisWeek = dayjs().week();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box bg="#222b37">
+      <Box
+        color="gray.200"
+        justifyContent="center"
+        alignItems="center"
+        display="flex"
+      >
+        <Text mr="5">Day of the Year:</Text>
+        <Box color="white" fontSize="3xl">
+          {today.toString()}
+        </Box>
+      </Box>
+      <Box
+        color="gray.200"
+        justifyContent="center"
+        alignItems="center"
+        display="flex"
+      >
+        <Text mr="5">Week of the Year:</Text>
+        <Box color="white" fontSize="3xl">
+          {thisWeek}
+        </Box>
+      </Box>
+    </Box>
   );
 }
 
